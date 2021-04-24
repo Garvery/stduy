66e76d786c13fe75eaf4a1ba068e1c7a873f5487
Garvery <18055929594@163.com>
2021-04-24 16:17:59 +0800

    first commit

---
+++ demo1.py
@@ -0,0 +1,23 @@
+# -*- codeding = uft-8 -*-
+# @Time :2021/4/23 23:17
+# @Author:fjw
+# @Site:
+# @File:demo1.py
+# @Software:PyCharm
+
+
+print("hello,world")  #这是单行注释
+
+'''
+这是第一行注释
+这是第二行注释
+'''
+
+a = 10
+print("这是变量:%s" %a)
+print("www","baidu","com",sep=".")
+print("hello ",end="")
+print("world",end="\t")
+print("python",end="\n")
+print("end")
+print("ss")
